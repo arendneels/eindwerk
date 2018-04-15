5ead540e71784d6f06bf7a31cbd1e137c3b2b3cf
Arend Neels <arendneels@hotmail.com>
2018-04-15 11:18:22 +0200

    Added faker

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,6 +13,40 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $faker = Faker\Factory::create();
+        DB::table('users')->insert([
+            'first_name' => $faker->firstName,
+            'last_name' => $faker->lastName,
+            'address' => $faker->streetAddress,
+            'city' => $faker->city,
+            'postal_code' => $faker->postcode,
+            'country_id' => 1,
+            'phone' => $faker->phoneNumber,
+            'email' => $faker->email,
+            'password' => bcrypt('123456'),
+        ]);
 
+        DB::table('users')->insert([
+            'first_name' => $faker->firstName,
+            'last_name' => $faker->lastName,
+            'address' => $faker->streetAddress,
+            'city' => $faker->city,
+            'postal_code' => $faker->postcode,
+            'country_id' => 1,
+            'phone' => $faker->phoneNumber,
+            'email' => $faker->email,
+            'password' => bcrypt('123456'),
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => $faker->firstName,
+            'last_name' => $faker->lastName,
+            'address' => $faker->streetAddress,
+            'city' => $faker->city,
+            'postal_code' => $faker->postcode,
+            'country_id' => 1,
+            'phone' => $faker->phoneNumber,
+            'email' => $faker->email,
+            'password' => bcrypt('123456'),
+        ]);
     }
 }
